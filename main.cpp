82816b8d972b54c03cbd26fc834fb1de7906bf9a
KsGin <imqqyangfan@gmail.com>
2018-09-04 09:38:37 +0800

    created framework

--- main.cpp
+++ main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include "headers/device.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    auto *device = new Device();
+
+    device->initialize(800, 600, false);
+    device->show();
+
+    while (!device->windowShouldClose()) {
+        device->handleEvent();
+    }
+
+    device->destory();
+    delete device;
+
     return 0;
 }
