7160e0c76c610abfb83d6caf5e93f5c109f17537
KsGin <imqqyangfan@gmail.com>
2018-09-07 13:54:51 +0800

    created raster class

---
+++ headers/raster.h
@@ -0,0 +1,18 @@
+/*
+ * File   : raster
+ * Author : KsGin 
+ * Date   : 2018/9/5
+ */
+
+#ifndef SOFTWARE_RENDERER_CPP_RASTER_H
+#define SOFTWARE_RENDERER_CPP_RASTER_H
+
+/**
+ * raster class
+ */
+class Raster {
+    
+};
+
+
+#endif //SOFTWARE_RENDERER_CPP_RASTER_H
---
+++ sources/raster.cpp
@@ -0,0 +1,7 @@
+/*
+ * File   : raster
+ * Author : KsGin 
+ * Date   : 2018/9/5
+ */
+
+#include "../headers/raster.h"
